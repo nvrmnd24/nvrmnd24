ac06c140e48ba15645b9c3402440e03eb4920dbd
nvrmnd24 <kzunirovckij@gmail.com>
2021-11-25 17:00:51 +0200

    Внесення змін до локального репозиторію 1

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <windows.h>
+
+using namespace std;
+int main()
+{
+	float Z, y;
+	cout << "Input y";
+	cin >> y;
+	Z = 1;
+	for (int m = 1; m <= 5; m++) {
+		Z = Z * (m * y);
+	}
+	y = sin(y) + 2 * Z / pow(y, 3) + 3;
+	cout << "Z=" << setprecision(3) << Z << endl;
+}
+
